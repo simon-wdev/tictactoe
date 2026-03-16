b5c23349cd01a35550c178335d1c2f2b610b9bd6
simon-wdev <simonw-dev@gmail.com>
2026-03-16 09:21:17 +0100

    Add comments
    Added more comments as a code review, for better understanding.

--- Skript/script.js
+++ Skript/script.js
@@ -1,9 +1,9 @@
 const gameBoard = (function(){ //Module Pattern
-    let playGround = ["","","","","","","","",""]
+    let playGround = ["","","","","","","","",""] //9 Felder, tictactoe, 3x3, am Anfang leer
 
     const getPlayGround = () => playGround;
 
-    const setPlayGround = (index, symbol) =>
+    const setPlayGround = (index, symbol) => //setzt später das Symbol an die Index Stelle
     {
             console.log("Symbol: " + symbol + " Index: " + index);
             playGround[index] = symbol;
@@ -22,13 +22,13 @@ const displayController = (function(){
         const board = gameBoard.getPlayGround(); //holt den Array
         const wrapper = document.querySelector(".game");//Div im HTML
 
-        wrapper.innerHTML = "";
+        wrapper.innerHTML = ""; //leert den Div, damit er nicht immer länger wird
 
-        board.forEach((element, index) => { 
+        board.forEach((element, index) => { //erstellt ein DIV für jedes Element, foreach bietet den Index integriert an
             const square = document.createElement("div");
-            square.classList.add("square");
-            square.textContent = element;
-            wrapper.appendChild(square);
+            square.classList.add("square"); //Klasse für CSS
+            square.textContent = element; //wenn sich das Element ändert, ändert sich auch der Text im DIV
+            wrapper.appendChild(square); //füg das div in den Wrapper ein
 
             square.addEventListener('click', () => {
                 gameController.playGame(index);  
@@ -39,7 +39,7 @@ const displayController = (function(){
     return {renderPlayGround};
 })();
 
-const  createPlayer = (name, symbol) => {
+const  createPlayer = (name, symbol) => { //returnt ein Objekt (Name und Symbol)
     return{
             playerName: name,
             playerSymbol: symbol
@@ -49,27 +49,27 @@ const  createPlayer = (name, symbol) => {
 
 const gameController = (function(){
 
-    const player1 = createPlayer("Player 1", "X");
+    const player1 = createPlayer("Player 1", "X"); //neuer Spieler in createPlayer
     const player2 = createPlayer("Player 2", "O");
     let currentPlayer = player1;
 
-    const playGame = (index, symbol) => {
+    const playGame = (index, symbol) => { //index kommt vom Eventlistener, integer aus der foreach Schleife
         if (currentPlayer === player1) {
-            gameBoard.setPlayGround(index, symbol)
-            displayController.renderPlayGround();
+            gameBoard.setPlayGround(index, player1.playerSymbol)
+            displayController.renderPlayGround(); //wird immer neu gerendert, deswegen muss der Div geleert werden
             console.log("Player 1 spielt mit " + symbol);
             changePlayer();
         }else if (currentPlayer === player2) {
-            gameBoard.setPlayGround(index, symbol)
+            gameBoard.setPlayGround(index, player2.playerSymbol)
             displayController.renderPlayGround();
             console.log("Player 2 spielt mit " + symbol);
             changePlayer();
-        }else{
+        }else{ //debug
             console.log("KEIN SPIELER DEFINIERT");
         }
     }
 
-    const changePlayer = () => {
+    const changePlayer = () => { //wechselt den Spieler, erstmal nur funktional
 
         if (currentPlayer === player1) {
             currentPlayer = player2;
@@ -80,4 +80,4 @@ const gameController = (function(){
     return {playGame, changePlayer};
 })();
 
-displayController.renderPlayGround();
+displayController.renderPlayGround();//rendert das Gameboard beim laden der Seite
