7a148627ec7ea95f83419a94c459a277aa7b1989
simon-wdev <simonw-dev@gmail.com>
2026-03-16 08:46:08 +0100

    Make player logic
    Constant change between players + changing the symbols every round.

--- Skript/script.js
+++ Skript/script.js
@@ -5,8 +5,9 @@ const gameBoard = (function(){ //Module Pattern
 
     const setPlayGround = (index, symbol) =>
     {
-        if (index>playGround.length) return; //wenn index länger Array, dann nichts
-            playGround[index]=symbol; //Index des Quadrats, ersetzt mit dem symbol (O, X), eventuell später nützlich
+            console.log("Symbol: " + symbol + " Index: " + index);
+            playGround[index] = symbol;
+            
     }
 
     return {getPlayGround, setPlayGround};
@@ -16,20 +17,67 @@ const gameBoard = (function(){ //Module Pattern
 
 
 const displayController = (function(){
+
     const renderPlayGround = () =>{
         const board = gameBoard.getPlayGround(); //holt den Array
         const wrapper = document.querySelector(".game");//Div im HTML
 
-        board.forEach(element => { //erstelle ein div für jedes ELement im Array
+        wrapper.innerHTML = "";
+
+        board.forEach((element, index) => { 
             const square = document.createElement("div");
             square.classList.add("square");
+            square.textContent = element;
             wrapper.appendChild(square);
-        });
 
-        
-    }
-        return {renderPlayGround};
+            square.addEventListener('click', () => {
+                gameController.playGame(index);  
+            }); 
+        });     
+    }; 
+
+    return {renderPlayGround};
 })();
 
+const  createPlayer = (name, symbol) => {
+    return{
+            playerName: name,
+            playerSymbol: symbol
+        }
+}
+
+
+const gameController = (function(){
+
+    const player1 = createPlayer("Player 1", "X");
+    const player2 = createPlayer("Player 2", "O");
+    let currentPlayer = player1;
+
+    const playGame = (index, symbol) => {
+        if (currentPlayer === player1) {
+            gameBoard.setPlayGround(index, symbol)
+            displayController.renderPlayGround();
+            console.log("Player 1 spielt mit " + symbol);
+            changePlayer();
+        }else if (currentPlayer === player2) {
+            gameBoard.setPlayGround(index, symbol)
+            displayController.renderPlayGround();
+            console.log("Player 2 spielt mit " + symbol);
+            changePlayer();
+        }else{
+            console.log("KEIN SPIELER DEFINIERT");
+        }
+    }
+
+    const changePlayer = () => {
+
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        }else if (currentPlayer === player2) {
+            currentPlayer = player1;
+        }
+    }
+    return {playGame, changePlayer};
+})();
 
 displayController.renderPlayGround();
